11ea048ce873eb62d781daf643daca687126cbcb
Oscar Raichert <oscarraichert@hotmail.com>
2023-11-10 01:16:59 -0300

    adicionando butao

--- src/meals/MealsScreen.tsx
+++ src/meals/MealsScreen.tsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Routes } from "../navigator/Routes";
-import { Button, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Entypo } from "@expo/vector-icons";
 
 export function MealsScreen() {
     const navigation = useNavigation<Routes>();
 
     return (
-        <View>
-            <Button title="New"></Button>
+        <View style={styles.body}>
+            <View style={styles.iconsRow}>
+                <Entypo.Button iconStyle={styles.icon} name="add-to-list" size={16} />
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    body: {
+        margin: "5%"
+    },
+
+    iconsRow: {
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+
+    icon: {
+        marginRight: 0
+    }
+});
